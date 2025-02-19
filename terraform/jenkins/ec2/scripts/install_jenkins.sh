1897141bfca854cad4af1b5ee1bb2576eec0c0bd
Sudheer Vadrevu <vadrevusudheer@gmail.com>
2025-02-19 09:44:52 +0100

    Add Dockerfile for Spring Boot web app and update Jenkins EC2 scripts

--- terraform/jenkins/ec2/scripts/install_jenkins.sh
+++ terraform/jenkins/ec2/scripts/install_jenkins.sh
@@ -2,7 +2,7 @@
 wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
 echo deb https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
 deb https://pkg.jenkins.io/debian-stable binary/
-        sudo apt-get update
+sudo apt-get update
 sudo apt-get install openjdk-8-jdk jenkins -y
-		sudo systemctl start jenkins
-		echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
+sudo systemctl start jenkins
+echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
